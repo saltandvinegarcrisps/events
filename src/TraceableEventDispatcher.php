dff47b9d2ade0c91b8a98bd08c06ae1910cd38cf
Kieron <kieron@fastmail.fm>
2019-10-21 09:43:42 +0100

    tracer dispatcher

---
+++ src/TraceableEventDispatcher.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Events;
+
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Psr\Log\LoggerInterface;
+
+class TraceableEventDispatcher implements EventDispatcherInterface
+{
+    /**
+     * @var EventDispatcherInterface
+     */
+    protected $eventDispatcher;
+
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    public function __construct(EventDispatcherInterface $eventDispatcher, LoggerInterface $logger)
+    {
+        $this->eventDispatcher = $eventDispatcher;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @inherit
+     */
+    public function dispatch(object $event)
+    {
+        $start = microtime(true);
+        $eventName = get_class($event);
+        $event = $this->eventDispatcher->dispatch($event);
+        $this->logger->info($eventName . ' dispatch completed', [
+            'duration' => microtime(true) - $start,
+            'event' => $eventName,
+        ]);
+        return $event;
+    }
+}
